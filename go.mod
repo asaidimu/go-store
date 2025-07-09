957d1a5cf1821783e87c2dcc0761ff13d7aba7aa
Saidimu <47994458+asaidimu@users.noreply.github.com>
2025-07-09 11:34:10 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/asaidimu/go-store
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/asaidimu/go-store/pkg"
+)
+
+func main() {
+	fmt.Println(pkg.Greeting("World"))
+}
